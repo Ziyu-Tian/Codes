0d3d24eb3dce416608e730960e2bb4e7163046c1
Ziyu_Tian <tianziyu0222@gmail.com>
2022-07-17 08:41:38 +0100

    Test change of C

--- Codes/C/Hello_World.cpp
+++ Codes/C/Hello_World.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    unsigned int x = 100, y = 3;
-    float result;
-    result = (float)x / y;
-    printf("%.3f",result);
-    //puts("Hello, world!/n");
+    puts("Hello, world!\n");
     return 0;
 }
--- Codes/C/Lab_1.1.cpp
+++ Codes/C/Lab_1.1.cpp
@@ -1,4 +1,4 @@
-//#define _CRT_SECURE_NO_WARNINGS
+
 #include<stdio.h>
 int main() {
 	int x, y, z, result;
