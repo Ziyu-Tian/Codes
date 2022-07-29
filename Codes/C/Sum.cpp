436867daecd562e4e15b9eb8cec99817be405930
Ziyu_Tian <tianziyu0222@gmail.com>
2022-07-29 20:43:50 +0100

    sum.cpp

--- Codes/C/Sum.cpp
+++ Codes/C/Sum.cpp
@@ -1,10 +1,27 @@
-#include<stdio.h>
-int main(){
-    int a,b,sum;
-    printf("Please input the numbers:");
-    scanf("%d %d",&a,&b);
-    sum=a+b;
-    printf("The sum is %d",sum);
-    return 0;
+#include <stdio.h>
+int sn(int n);
+int main()
+{
+    int n;
+    int sum=0;
+    scanf("%d", &n);
+    for (int i = 1; i <=n; i++)
+    {
+        sum+=sn(i);
+    }
     
+    printf("%d", sum);
+    return 0;
+}
+int sn(int n)
+{
+
+    if (n == 1)
+    {
+        return 2;
+    }
+    else
+    {
+        return sn(n - 1) * 10 + 2;
+    }
 }
