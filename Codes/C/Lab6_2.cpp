7bd8cfe1b17fed070e76eb13e870765fbc759d69
Ziyu_Tian <tianziyu0222@gmail.com>
2022-08-01 09:17:22 +0100

    sizeof_try

--- Codes/C/Lab6_2.cpp
+++ Codes/C/Lab6_2.cpp
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <string.h>
 void sort(char str[]);
@@ -33,8 +34,29 @@ int main()
     sort(s);
     for (int i = 0; i < stringlength(s); i++)
     {
-        printf("%c",s[i]);
+        printf("%c ",s[i]);
     }
     return 0;
     
 }
+*/
+#include <stdio.h>
+#define SIZE 20
+
+size_t getSize(float *ptr); // prototype
+
+int main(void)
+{
+   float array[SIZE]; // create array
+
+   printf("The number of bytes in the array is %lu"            
+          "\nThe number of bytes returned by getSize is %lu\n",
+          sizeof(array), getSize(array));                
+} 
+
+// return size of ptr
+size_t getSize(float *ptr)
+{
+   return sizeof(ptr);
+}
+
