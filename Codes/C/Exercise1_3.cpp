547ee55e24d75a69986ea69e9b4b0ad364fabdc1
Ziyu_Tian <tianziyu0222@gmail.com>
2022-10-08 07:49:53 +0100

    update

--- Codes/C/Exercise1_3.cpp
+++ Codes/C/Exercise1_3.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<cstring>
-#include<cstdlib>
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
 using namespace std;
 
 int main()
@@ -9,50 +9,30 @@ int main()
     int Line;
     seed = time(0);
     srand(seed);
-    cout<<"Please input the dimension of the matrix:"<<endl;
-    cin>>Line;
+    cout << "Please input the dimension of the matrix:" << endl;
+    cin >> Line;
     double a[Line][Line];
-    cout<<"Initializing the matrix..."<<endl;
+    cout << "Initializing the matrix..." << endl;
     for (int i = 0; i < Line; i++)
     {
         for (int j = 0; j < Line; j++)
         {
-            a[i][j] = rand()%10;
+            a[i][j] = rand() % 10;
         }
     }
     for (int i = 0; i < Line; i++)
     {
         for (int j = 0; j < Line; j++)
         {
-            printf("%.2f ",a[i][j]);
+            printf("%.2f ", a[i][j]);
         }
         printf("\n");
-        
     }
-    
-    return 0;
 
+    return 0;
 }
 
-double det (int a[][N],int n,int d) // n is the order of matrix, d is the line/column
-// to be deleted
+void inverse(int a[][],int n)// n is the order of the matrix
 {
-    if (n==1)
-    {
-        return a[0][0]; // the exit of the recursive
-    }
-    else
-    {
-        for (int i = 0; i < n; i++)
-        {
-            int New[N][N];
-            
-        }
-        
-        
 
-
-    }
-    
-    
 }
