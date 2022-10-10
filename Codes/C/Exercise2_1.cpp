1c75fa385b0470114e35112ce0e4874a40f329cd
Ziyu_Tian <tianziyu0222@gmail.com>
2022-10-10 20:28:24 +0100

    Ex2.1

---
+++ Codes/C/Exercise2_1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void do_encrypt(char m[]);
+void display_message(char m[]);
+int main()
+{
+   char m[100];
+   cout<<"Please input your message:"<<endl;
+   cin.get(m,100);// cin.get can store the space
+   cout<<"The original message:"<<endl;
+   display_message(m);
+   do_encrypt(m);
+   cout<<"The encrypted message:"<<endl;
+   display_message(m);
+   return 0;
+}
+void do_encrypt(char m[])
+{
+    int size = strlen(m);
+    for (int i = 0; i < size; i++)
+    {
+       m[i] = ((m[i] * 9) % 96) + 31;
+    }
+}
+void display_message(char m[])
+{
+    
+    cout<<m<<endl;
+}
