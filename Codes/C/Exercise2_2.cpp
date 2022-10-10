5af7d8945bea91791e613f7441882a056979200e
Ziyu_Tian <tianziyu0222@gmail.com>
2022-10-10 22:31:37 +0100

    ex2.2

---
+++ Codes/C/Exercise2_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    string s_1 = "";// a new string to store the sentence
+    getline(cin, s);
+    int size = s.length();//get the length of the string 
+    for (int i = 0; i<size; i++)
+    {
+        if (s[i] != ' ')//not space, append that character to s_1
+        {
+            s_1.append(1, s[i]);
+        }
+            
+        else if (s[i + 1] != ' ') // check if next bit is space
+            s_1.append(1, s[i]);
+            // if the next is also space, i++ until s[i+1] is not space
+            // if the next is not space, the previous space will be appended
+    }
+    cout<<s_1<<endl;
+    return 0;
+}
