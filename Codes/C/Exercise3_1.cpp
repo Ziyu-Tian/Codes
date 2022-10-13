1ac8680849450f6065a88dee140081c6753cc7bb
Ziyu_Tian <tianziyu0222@gmail.com>
2022-10-13 16:05:01 +0100

    Fib

---
+++ Codes/C/Exercise3_1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int fib(int n);
+int main()
+{
+    int n; //setting the number of lines
+    cout<<"Please input the number:"<<endl;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            /* code */
+        }
+        
+        cout<<"><((º> "
+    }
+    
+
+}
+int fibFish(int n)
+{
+    if (n ==1)
+    {
+        cout<<"><((º> ";;
+    }
+    else if (n==2)
+    {
+        cout<<"><((º> "<<endl;
+    }
+    else
+    {
+        return Fib(n-1)+Fib(n-2);
+    }
+       
+}
