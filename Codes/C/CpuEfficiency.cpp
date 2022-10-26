1a71f82330db9a3bd029600a9ce511100431a2e9
Ziyu <tianziyu0222@163.com>
2022-10-26 20:54:31 +0100

    CPU First Version

--- Codes/C/CpuEfficiency.cpp
+++ Codes/C/CpuEfficiency.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <ctime>
 #include <unistd.h>
+#include <string.h>
+#include <iomanip>
 using namespace std;
 void clearScreen();
 
@@ -13,15 +15,9 @@ int main()
     ifstream file("/proc/stat");
     string line;
     string word;
-    int i = 0;
-    long long unsigned int data[20][20];
-    for (int m = 0; m < 20; m++)
-    {
-        for (int n = 0; n < 20; n++)
-        {
-            data[m][n] = 0;
-        }
-    }
+    long long unsigned int sum = 0;
+    float percentage[20][20];
+    memset(percentage, 0, sizeof(percentage));
 
     if (!file.good())
     {
@@ -30,31 +26,48 @@ int main()
     }
 
     clearScreen();
-
-
-    while (getline(file, line))
+    while (true)
     {
-        stringstream linestream(line);
-        string token;
-        getline(linestream, token, ' ');
-        long long unsigned int user = 0;
-        long long unsigned int nice = 0;
-        long long unsigned int system = 0;
-        long long unsigned int idle = 0;
-
-        if (token[0] == 'c' && token[1] == 'p' && !(token == "cpu"))
+        int i = 0;
+        while (getline(file, line))
         {
+            stringstream linestream(line);
+            string token;
+            getline(linestream, token, ' ');
+            long long unsigned int user = 0;
+            long long unsigned int nice = 0;
+            long long unsigned int system = 0;
+            long long unsigned int idle = 0;
+            long long unsigned int iowait = 0;
+            long long unsigned int irq = 0;
+            long long unsigned int softirq = 0;
 
-            linestream >> user >> nice >> system >> idle;
-            data[i][0] = user;
-            data[i][1] = nice;
-            data[i][2] = system;
-            data[i][3] = idle;           
-            ++i;
+            if (token[0] == 'c' && token[1] == 'p' && !(token == "cpu"))
+            {
+                if (i > 3)
+                {
+                    break;
+                }
+                else
+                {
+                    linestream >> user >> nice >> system >> idle >> iowait >> irq >> softirq;
+                    sum = user + nice + system + idle + iowait + irq + softirq;
+                    percentage[i][0] = (float)user * 100 / sum;
+                    percentage[i][1] = (float)idle * 100 / sum;
+                    percentage[i][2] = (float)system * 100 / sum;
+                    percentage[i][3] = (float)nice * 100 / sum;
+                    cout << fixed << setprecision(6)
+                         << percentage[i][0] << "%\t"
+                         << percentage[i][1] << "%\t"
+                         << percentage[i][2] << "%\t"
+                         << percentage[i][3] << "%" << endl;
+                    ++i;
+                }
+            }
         }
-        //usleep(500000);
-        //clearScreen();
-        /*
+        usleep(500000);
+        clearScreen();
+
         file.close();
         file.open("/proc/stat");
         if (!file.good())
@@ -62,70 +75,9 @@ int main()
             cerr << "Could not open file.... exitting..." << endl;
             exit(EXIT_FAILURE);
         }
-        */
     }
     file.close();
-
-// while (true)
-//{
-/*
-while (getline(file, line))
-{
-    stringstream linestream(line);
-    stringstream stringsum;
-    string token;
-    string word;
-
-    long long unsigned int user = 0;
-    long long unsigned int nice = 0;
-    long long unsigned int system = 0;
-    long long unsigned int idle = 0;
-    getline(linestream, token, ' ');
-    if (token[0] == 'c' && token[1] == 'p' && !(token == "cpu"))
-    {
-        cout<<token<<endl;
-
-
-        while (linestream >> word >> user >> nice >> system >> idle)
-        {
-            data[i][0] = user;
-            data[i][1] = nice;
-            data[i][2] = system;
-            data[i][3] = idle;
-        }
-    }
-     usleep(500000);
-     clearScreen();
-    file.close();
-
-    file.open("/proc/stat");
-    if (!file.good())
-    {
-        cerr << "Could not open file.... exitting..." << endl;
-        exit(EXIT_FAILURE);
-    }
-    }
-    file.close();
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            cout << data[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-}
-*/
-for (int i = 0; i < 3; i++)
-{
-    for (int j = 0; j < 4; j++)
-    {
-        cout << data[i][j] << " ";
-    }
-    cout << endl;
-}
-return 0;
+    return 0;
 }
 
 void clearScreen()
