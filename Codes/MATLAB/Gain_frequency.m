405dd82b1a5441b33386c42de6d631ac39a14428
Ziyu_Tian <tianziyu0222@gmail.com>
2022-12-08 19:28:56 +0000

    Matlab Update

--- Codes/MATLAB/Gain_frequency.m
+++ Codes/MATLAB/Gain_frequency.m
@@ -1,2 +1,22 @@
-data_1 = readtable('gainFrequency,xlsx',1,'A2:B8');
-data_2 = readtable('gainFrequency.xlsx',2,'A2:B9');
+data_1 = xlsread('gainFrequency.xlsx',1,'A2:D8');
+data_2 = xlsread('gainFrequency.xlsx',2,'A2:D9');
+
+x_1 = data_1(:,1);
+x_1 = 2*log10(x_1);
+
+x_2 = data_1(:,2);
+
+y_1 = data_1(:,4);
+y_1 = 20*log10(y_1);
+
+y_2 = data_2(:,4);
+y_2 = 20*log10(y_2);
+
+
+plot(x_1,y_1,'b-');
+
+xlabel('Frequency, dB(kHz)');
+ylabel('Gain, dB');
+title('Gain versus Frequency');
+
+pause(500);
