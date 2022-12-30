26cada55dbba541d1b3cdb55c2e129cc08a89ca5
Ziyu_Tian <tianziyu0222@gmail.com>
2022-12-30 21:37:10 +0000

    Update

---
+++ Codes/C/Exercise_11.1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "lcdCalculate.hpp"
+using namespace std;
+
+int main()
+{
+    lcd(3,4,5,15);
+    return 0;
+}
+
---
+++ Codes/C/Exercise_11.2.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#include "lcdCalculate.hpp"
+using namespace std;
+
+int main()
+{
+    cout << lcd_2(4,5,15) << endl;
+    return 0; 
+}
