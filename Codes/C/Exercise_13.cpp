7091e74ac6a09cae5ed21285d98b20356be951b5
Ziyu_Tian <tianziyu0222@gmail.com>
2022-12-31 10:03:28 +0000

    Exercise_13

---
+++ Codes/C/Exercise_13.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#include "piCalculation.hpp"
+using namespace std;
+#define Radius 6377768 // define the constant 
+int main()
+{
+    cout << 2 * Pi(12) * Radius << endl; // call the Pi function of 12 terms
+    return 0;
+}
