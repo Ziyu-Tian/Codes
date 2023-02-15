e0dcd71f44684e8dbf3c0b8ed8ca2d31b8b13de6
Ziyu_Tian <tianziyu0222@gmail.com>
2023-02-15 09:03:16 +0000

    matlab

--- Codes/MATLAB/Function.m
+++ Codes/MATLAB/Function.m
@@ -1,7 +1,8 @@
 syms x;
-f_1=@(x)atan(6./x)-atan(4./x);
+f_1= atan(6./x)-atan(4./x);
+f_2=rad2deg(f_1);
 figure (1);
-fplot(f_1,[0,100],'r');
+fplot(f_2,[0,100],'r');
 grid on;
 xlabel('x');
 ylabel('y');
