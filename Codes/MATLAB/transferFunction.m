91bdc49821536b5c26ddbb23b5a060fdc1951808
Ziyu_Tian <tianziyu0222@gmail.com>
2023-03-17 12:54:59 +0000

    MATLAB

--- Codes/MATLAB/transferFunction.m
+++ Codes/MATLAB/transferFunction.m
@@ -1,25 +1,18 @@
 
-den = poly([-1 -2 -3]);
-figure(1);
-g_1 = tf(1,den);
-g_2 = tf(10,den);
-g_3 = tf(100,den);
-gcl = feedback(g_1, 1);
-gcl_2 = feedback(g_2, 1);
-gcl_3 = feedback(g_3, 1);
-figure(1);
-step(gcl);
-hold on
-step(gcl_2);
-figure(2);
-hold on
-step(gcl_3);
+g_1 = tf(1,[2 2 2]);
+g_2 = tf(1, [1 6 11 6]);
+g_3 = tf([1 2 2],[1 6 11 6]);
 %step(gcl,'-b');
 %hold on
 %step(gcl_2,'-r');
 %hold on
 %step(gcl_3,'-g');
 grid on;
+
+
+figure(2);
+
+rlocus(g_3);
 
 %{
 figure(2);
