536947b1cf7ea4c9631d125da126067a68094aca
Ziyu_Tian <tianziyu0222@gmail.com>
2023-03-19 16:56:17 +0000

    Arduino LED Test

---
+++ Codes/Arduino/control/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+int led = 13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led,HIGH);
+  delay(2000);
+  digitalWrite(led,LOW);
+  delay(2000);
+}
