85089f6854125b1d271764f1b6a96b0b266e6a79
Ziyu_Tian <tianziyu0222@gmail.com>
2023-03-21 08:52:20 +0000

    pid

---
+++ Codes/Arduino/control/lib/Steering/Steering.cpp
@@ -0,0 +1,5 @@
+#include "Steering.hpp"
+
+Servo myServo;
+
+
--- Codes/Arduino/control/src/main.cpp
+++ Codes/Arduino/control/src/main.cpp
@@ -1,14 +1,41 @@
 #include <Arduino.h>
-int led = 13;
-void setup() {
+#include <Servo.h>
+#include "Sesnor.hpp"
+Servo myServo;
+#define rightSensor A0
+#define leftSensor A1
+#define servoPWM 9
+struct PID
+{
+  float Kp;
+  float Ki;
+  float Kd;
+};
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(led,OUTPUT);
+  myServo.attach(servoPWM);
+  //Serial.begin(9600);
+  pinMode(leftSensor, INPUT);
+  pinMode(rightSensor, INPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  digitalWrite(led,HIGH);
+
+  float deviation = sensorRead();
+  // read the difference between the left and right sensor 
+
+  /*
+  myServo.writeMicroseconds(1200);
   delay(2000);
-  digitalWrite(led,LOW);
+  myServo.writeMicroseconds(1500);
   delay(2000);
+  myServo.writeMicroseconds(1800);
+  delay(2000);
+  */
+ 
+  Serial.println(deviation);
+  delay(1000);
 }
