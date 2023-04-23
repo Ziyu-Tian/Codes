d92864ec418d398548c9e612e7b60cfe4b347be3
Ziyu_Tian <tianziyu0222@gmail.com>
2023-04-23 15:32:36 +0100

    matlab

--- Codes/MATLAB/Function.m
+++ Codes/MATLAB/Function.m
@@ -1,11 +1,17 @@
-syms t;
-f_1= -2*cos(t)+2*sin(t);
+syms c;
+f_1= -3/2*c+1;
+f_2= -6*c+8;
+f_3= -(27/2)*c+27;
 %f_2=rad2deg(f_1);
 figure (1);
-fplot(f_1,[0,1000],'r');
+fplot(f_1,[0,100],'r');
 grid on;
-xlabel('t');
-ylabel('x');
+hold on
+fplot(f_2,[0,100],'b');
+hold on
+fplot(f_3,[0,1000],'k');
+xlabel('c');
+ylabel('y');
 pause(200);
 
 
