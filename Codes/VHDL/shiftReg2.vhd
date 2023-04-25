c8c65d6ae7e78a3a4c6a482ec002ac9596a12a1e
Ziyu_Tian <tianziyu0222@gmail.com>
2023-04-25 13:38:36 +0100

    shiftReg2: 8 bit condition

--- Codes/VHDL/shiftReg2.vhd
+++ Codes/VHDL/shiftReg2.vhd
@@ -6,17 +6,17 @@ USE IEEE.std_logic_arith.ALL;
 ENTITY reg_2 IS
 
     PORT (
-        r2CLK, r2CLR, carry : IN STD_LOGIC;
+        r2CLK, r2CLR: IN STD_LOGIC;
         r2in : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-        Cout : OUT STD_LOGIC;
-        r2OUT : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+	carry : IN STD_LOGIC;
+        r2OUT : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
     );
 
 END reg_2;
 
 ARCHITECTURE rtl OF reg_2 IS
 
-    SIGNAL reg2 : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL reg2 : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
 BEGIN
     reg : PROCESS (r2CLK, r2CLR)
@@ -25,12 +25,10 @@ BEGIN
         IF (r2CLR = '1') THEN
             reg2 <= (OTHERS => '0');
         ELSIF (r2CLK'event AND r2CLK = '1') THEN
-            reg2(3)<= carry;
-	    reg2(2)<= r2in(3);
-	    reg2(1)<= r2in(2);
-	    reg2(0)<= r2in(1);
+	    reg2(2 downto 0)<= reg2(3 downto 1);
+	    reg2(6 downto 3)<=r2in;
+	    reg2(7) <= carry;
         END IF;
     END PROCESS;
     r2OUT <= reg2;
-    Cout <= r2in(0);
 END rtl;
