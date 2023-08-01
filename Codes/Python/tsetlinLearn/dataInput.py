d0b8beb78c97ea29fef8085dc858d36bac0a0bc0
Ziyu_Tian <tianziyu0222@163.com>
2023-08-01 13:24:56 +0000

    binarizer_init

--- Codes/Python/tsetlinLearn/dataInput.py
+++ Codes/Python/tsetlinLearn/dataInput.py
@@ -3,8 +3,8 @@ import pandas as pd
 from binarizer import StandardBinarizer
 
 
-x = pd.read_csv('IRIS.csv',usecols=['sepal_length','sepal_width','petal_length','petal_width'])
-y = pd.read_csv('IRIS.csv',usecols=['species'])
+x = pd.read_csv('mnist_train.csv',usecols=range(1, 784))
+y = pd.read_csv('mnist_train.csv',usecols=['label'])
 x = np.array(x)
 y = np.array(y)
 #print(x)
