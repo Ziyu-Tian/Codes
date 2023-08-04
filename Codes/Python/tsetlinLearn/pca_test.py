8381e493a0d10c744159dc35a9b94eb08cf70aee
Ziyu_Tian <tianziyu0222@gmail.com>
2023-08-04 21:27:22 +0100

    update in local

--- Codes/Python/tsetlinLearn/pca_test.py
+++ Codes/Python/tsetlinLearn/pca_test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.datasets import fetch_openml
-x = pd.read_csv('train.csv',usecols=range(0, 3461))
+x = pd.read_csv('train.csv',usecols=range(0, 3461),header = None)
 
 X = np.array(x) 
 
@@ -13,14 +13,19 @@ pca.fit(X)
 PCA(n_components=0.95)
 
 num = pca.n_components_
+"""
+print(num)
+"""
 
 n_components = num
 pca = PCA(n_components=n_components)
 
 X_pca = pca.fit_transform(X)
 
+print(X_pca)
 print(X_pca.shape)
-print(pca.explained_variance_ratio_)
+
+#print(pca.explained_variance_ratio_)
 
 
 
