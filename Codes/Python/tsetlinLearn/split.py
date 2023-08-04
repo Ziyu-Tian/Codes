02ca980c1c71bc51b5df39e1f566f86593a1d865
Ziyu_Tian <tianziyu0222@gmail.com>
2023-08-04 08:01:27 +0100

    update in local

--- Codes/Python/tsetlinLearn/split.py
+++ Codes/Python/tsetlinLearn/split.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('output.csv',header=None)
 
-part1 = df.iloc[:60000]
-part2 = df.iloc[60000:70000]
+part1 = df.iloc[:500]
+part2 = df.iloc[60000:60100]
 
 part1.to_csv('train.csv',index = False, header=None)
 part2.to_csv('test.csv', index = False, header=None)
