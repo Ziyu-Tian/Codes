a9e3aaa02718cc96c98395831abd50607cd0a380
Ziyu_Tian <tianziyu0222@gmail.com>
2024-05-03 19:05:45 +0100

    upload_graph

--- Codes/Python/pytest/LUT_graph.py
+++ Codes/Python/pytest/LUT_graph.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 plt.rc('font',family='Times New Roman')
 x_data = ['Xor','Iris','Noisy_Xor']
-ax_data = [420,494,424]
-ax1_data = [0.79,0.93,0.80]
+ax_data = [420,494,427]
+ax1_data = [545,561,557]
 
 fig=plt.figure(dpi=600)
 ax=fig.add_subplot(111)
 
-ax.set_ylim([400,510])
-ax.set_yticks=np.arange(400,510)
-ax.set_yticklabels=np.arange(400,510)
+ax.set_ylim([400,510])#range of left-y
+ax.set_yticks=np.arange(400,510) #range of left-y
+ax.set_yticklabels=np.arange(400,510) #range of left-y
 
 bar_width = 0.3
 ax.set_ylabel('LUT Numbers',fontsize=10,fontweight='bold');
@@ -22,11 +22,11 @@ for a,b in enumerate(ax_data):
     
 ax1 = ax.twinx() # this is the important function   
 
-ax1.set_ylim([0.6,1])
-ax1.set_yticks=np.arange(0.6,1)
-ax1.set_yticklabels=np.arange(0.6,1)
-ax1.set_ylabel('LUT Ratio (%)',fontsize=10,fontweight='bold');
-lns2=ax1.bar(x=np.arange(len(x_data))+bar_width, width=bar_width, height=ax1_data,label='LUT Ratio (%)',fc = 'indianred',alpha=0.8)
+ax1.set_ylim([530,600]) #range of right-y
+ax1.set_yticks=np.arange(530,600) #range of right-y
+ax1.set_yticklabels=np.arange(530,600) #range of right-y
+ax1.set_ylabel('FF Numbers',fontsize=10,fontweight='bold');
+lns2=ax1.bar(x=np.arange(len(x_data))+bar_width, width=bar_width, height=ax1_data,label='FF Numbers',fc = 'indianred',alpha=0.8)
 
 for a,b in enumerate(ax1_data):
     plt.text(a+0.3,b+0.001,'%s' % b,ha='center')
@@ -37,4 +37,4 @@ ax.set_xlabel('Dataset Type',fontsize=10,fontweight='bold')
 
 fig.legend(loc=1,bbox_to_anchor=(0.28,1),bbox_transform=ax.transAxes)
 
-plt.savefig('./name.png')  #图表输出
+plt.savefig('./LUT.png')  #图表输出
---
+++ Codes/Python/pytest/LUT_graph2.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.rc('font',family='Times New Roman')
+x_data = ['Original Design','Optimized Design']
+ax_data = [494,630]
+ax1_data = [0.992,2.469]
+
+fig=plt.figure(dpi=600)
+ax=fig.add_subplot(111)
+
+ax.set_ylim([400,650])#range of left-y
+ax.set_yticks=np.arange(400,650) #range of left-y
+ax.set_yticklabels=np.arange(400,650) #range of left-y
+
+bar_width = 0.3
+ax.set_ylabel('LUT Numbers',fontsize=10,fontweight='bold');
+lns1=ax.bar(x=np.arange(len(x_data)), width=bar_width, height=ax_data, label='LUT Numbers', fc = 'steelblue',alpha=0.8)
+
+for a,b in enumerate(ax_data):
+    plt.text(a,b+0.0005,'%s' % b,ha='center')
+    
+    
+ax1 = ax.twinx() # this is the important function   
+
+ax1.set_ylim([0.5,3]) #range of right-y
+ax1.set_yticks=np.arange(0.5,3) #range of right-y
+ax1.set_yticklabels=np.arange(0.5,3) #range of right-y
+ax1.set_ylabel('Power (W)',fontsize=10,fontweight='bold');
+lns2=ax1.bar(x=np.arange(len(x_data))+bar_width, width=bar_width, height=ax1_data,label='Power (W)',fc = 'indianred',alpha=0.8)
+
+for a,b in enumerate(ax1_data):
+    plt.text(a+0.3,b+0.001,'%s' % b,ha='center')
+    
+plt.xticks(np.arange(len(x_data))+bar_width/2, x_data)
+
+ax.set_xlabel('Power / Memory of Design-1 vs Design2',fontsize=10,fontweight='bold')
+
+fig.legend(loc=1,bbox_to_anchor=(0.28,1),bbox_transform=ax.transAxes)
+
+plt.savefig('./LUT_Power.png')  #图表输出
