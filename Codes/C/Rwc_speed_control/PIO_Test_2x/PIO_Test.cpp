f393dfcdf4abd33def27c0ba457afd2f90df2e6b
Ziyu_Tian <tianziyu0222@gmail.com>
2025-03-02 17:20:01 +0000

    adding comments for PIO_Test

--- Codes/C/Rwc_speed_control/PIO_Test_2x/PIO_Test.cpp
+++ Codes/C/Rwc_speed_control/PIO_Test_2x/PIO_Test.cpp
@@ -9,31 +9,32 @@ int main() {
     
     stdio_init_all();
 
-    // 
-    uint8_t A_pin = 23; // 
+    // Pin_A (Pin_B would be set as A_pin + 1)
+    uint8_t A_pin = 23; // Noted that A and B should be swapped in physical connection
     float ppr = 600.0; // 
 
-    // 
+    // Initialize PIO_Encoder Class
     QuadratureEncoder encoder(A_pin, ppr);
 
-    // 
+    // Sampling Time for Speed Calculation
     float sampling_time = 1e-3;
 
     while (true) {
-        // 
-        encoder.update(sampling_time);
-
-        // 
+        
+        encoder.update(sampling_time); // Initialize starting time
+            
+        // Position: return angle (default radium)
+        // Velocity: return velocity in rad/s
+        // Counter: return counting number (CW++, ACW--, 1200 per round)
         auto position = encoder.get_position();
         auto velocity = encoder.get_velocity();
         auto counter = encoder.get_count();
 
         // 
-        // 
         printf("Position: %f, Velocity: %f\n, Counter: %d\n", position, velocity, counter);
 
         // 
-        sleep_ms(500);
+        sleep_ms(1000);
     }
 
     return 0;
