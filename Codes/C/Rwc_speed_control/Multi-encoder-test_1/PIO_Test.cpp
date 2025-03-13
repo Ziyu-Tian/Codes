f84fd80aedce4dbfbaa406646fd455e4bd2ce44b
Ziyu_Tian <tianziyu0222@gmail.com>
2025-03-13 08:24:12 +0000

    update

---
+++ Codes/C/Rwc_speed_control/Multi-encoder-test_1/PIO_Test.cpp
@@ -0,0 +1,95 @@
+/*
+GPIO directly connection via PIO
+PPR captured with 2x frequency, measured as 1200 correctly
+*/
+#include <QuadratureEncoder.hpp>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include <stdio.h>
+
+// SPI PIN Config
+#define SPI_PORT spi0   // spi0 applied
+#define PIN_SCK  2      // SCK = GPIO2
+#define PIN_MOSI 3      // MOSI = GPIO3
+#define PIN_MISO 4      // MISO
+#define PIN_CS   8      // CS
+
+// SPI Init Function 
+void init_spi() {
+    spi_init(SPI_PORT, 1000 * 1000); // 1Mhz SPI 
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+
+    gpio_init(PIN_CS);
+    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_put(PIN_CS, 1); // Unselect chip
+}
+
+// Sending SPI cmd + data 
+void write_pot(uint8_t cmd, uint8_t value) {
+    uint8_t data[2] = {cmd, value};
+
+    gpio_put(PIN_CS, 0); // Select chip, start sending data
+    spi_write_blocking(SPI_PORT, data, 2);
+    gpio_put(PIN_CS, 1); // Unselect chip, end data sending
+}
+
+int main() {
+    
+    stdio_init_all();
+    init_spi();
+
+    // Pin_A (Pin_B would be set as A_pin + 1)
+    uint8_t A_pin = 23; // Noted that A and B should be swapped in physical connection
+    uint8_t A2_pin = 21;
+    uint8_t A3_pin = 19;
+    float ppr = 600.0; // 
+
+    // Initialize PIO_Encoder Class
+    QuadratureEncoder encoder_1(A_pin, ppr);
+    QuadratureEncoder encoder_2(A3_pin, ppr);
+    //QuadratureEncoder encoder_3(A2_pin, ppr);
+    // Sampling Time for Speed Calculation
+    float sampling_time = 100e-3;
+
+    // Digital Pot settings
+    int digital_pot_output = 10; // 0 - 255 (-100% to +100%)
+
+    while (true) {
+        
+        encoder_1.update(sampling_time); // Initialize starting time
+        encoder_2.update(sampling_time); // Initialize starting time 
+        // encoder_3.update(sampling_time); // Initialize starting time 
+        // Position: return angle (default radium)
+        // Velocity: return velocity in rad/s
+        // Counter: return counting number (CW++, ACW--, 1200 per round)
+        auto position = encoder_1.get_position();
+        auto velocity = encoder_1.get_velocity();
+        auto counter = encoder_1.get_count();
+
+        auto position_2 = encoder_2.get_position();
+        auto velocity_2 = encoder_2.get_velocity();
+        auto counter_2 = encoder_2.get_count();
+
+        // auto position_3 = encoder_3.get_position();
+        // auto velocity_3 = encoder_3.get_velocity();
+        // auto counter_3  = encoder_3.get_count();
+        
+        float rpm_1 = (15 * velocity) / M_PI;
+        float rpm_2 = (15 * velocity_2) / M_PI;
+        //float rpm_3 = (15 * velocity_3) / M_PI;
+
+        write_pot(0x11, digital_pot_output); 
+
+        // printf("Velocity_1: %.2f RPM , Counter_1: %d ;", rpm_1, counter);
+        // printf("Velocity_2: %.2f RPM , Counter_2: %d\n", rpm_2, counter_2);
+        printf("RPM_1: %.2f, ",velocity);
+        printf("RPM_2: %.2f, ",velocity_2);
+        //printf("RPM_3: %.2f\n",velocity_3);
+        sleep_ms(500);
+    }
+
+    return 0;
+
+}
---
+++ Codes/C/Rwc_speed_control/Multi-encoder-test_1/pico-quadrature-encoder/src/QuadratureEncoder.cpp
@@ -0,0 +1,57 @@
+#include "QuadratureEncoder.hpp"
+#include <stdio.h>
+
+QuadratureEncoder::QuadratureEncoder(uint8_t pin, float ppr, float gear_ratio) : ppr(ppr * 4.0f), gear_ratio(gear_ratio)
+{
+    pio_instance = pio0;
+    sm = pio_claim_unused_sm(pio_instance, true);
+    if (sm == -1)
+        pio_instance = pio1;
+    sm = pio_claim_unused_sm(pio_instance, true);
+    if (sm == -1)
+    printf("Error: No free state machine found!\n");
+    uint offset = pio_add_program(pio_instance, &quadrature_encoder_program);
+    quadrature_encoder_program_init(pio_instance, sm, pin, 0);
+
+}
+
+
+void QuadratureEncoder::update(float dt)
+{
+    _last_count = _count;
+    _count = quadrature_encoder_get_count(pio_instance, sm);
+    _dt = dt;
+}
+
+int32_t QuadratureEncoder::get_count()
+{
+    return _count;
+}
+
+float QuadratureEncoder::get_position(bool degrees)
+{
+    if (degrees)
+        return 360.0f * _count / (ppr * gear_ratio); // deg
+    else
+        return 2 * M_PI * _count / (ppr * gear_ratio); // rad
+}
+
+float QuadratureEncoder::get_velocity(bool degrees)
+{
+    float velocity = ((_count - _last_count) / (ppr * gear_ratio)) / _dt;
+
+    if (degrees)
+        return 360.0f * velocity; // deg/s
+    else
+        return 2 * M_PI * velocity; // rad/s
+}
+
+bool QuadratureEncoder::get_direction()
+{
+    return _count > _last_count ? 1 : -1;
+}
+
+int8_t QuadratureEncoder::get_sign()
+{
+    return (_count > 0) - (_count < 0);
+}
